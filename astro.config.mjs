d2fc036603e90687df75e21df6b16f0e01d4b058
williamgurzoni <william_luizat@hotmail.com>
2024-11-16 17:25:24 +1300

    Update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gurzoni.dev',
+    site: 'https://williamgurzoni.github.io/me/',
     integrations: [
         mdx(),
         sitemap(),
